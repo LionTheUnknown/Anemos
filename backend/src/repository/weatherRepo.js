7042076a1cd38014af57c4131b09106875df181f
Lino <116647405+LionTheUnknown@users.noreply.github.com>
2026-02-08 23:06:18 +0200

    base url naming change

--- backend/src/repository/weatherRepo.js
+++ backend/src/repository/weatherRepo.js
@@ -1,11 +1,10 @@
 import pg from 'pg';
 
 const { Pool } = pg;
-console.log("DATABASE_URL:", !!process.env.DATABASE_URL);
 
-const pool = process.env.DATABASE_URL
+const pool = process.env.POSTGRES_URL
   ? new Pool({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: process.env.POSTGRES_URL,
       ssl: { rejectUnauthorized: false },
     })
   : new Pool({
